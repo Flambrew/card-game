74a0d10e36d5425b8ce7284ee1a1a746503bc457
flambrew <matherne.a4@gmail.com>
2025-07-31 18:08:17 -0400

    device sync

--- scripts/entities/enemies/unnamed_enemy_0/enemy_character.gd
+++ scripts/entities/enemies/unnamed_enemy_0/enemy_character.gd
@@ -14,24 +14,15 @@ func setup():
 	set_target(global_position)
 
 func navigate(delta:float) -> void:
-	print("pos: ", position, ",\t->\ttarget: ", nav_agent.target_position)
 	if nav_agent.is_navigation_finished():
 		return
 	
 	var current_agent_position:Vector3 = global_position
-	print("current_agent_position: ", current_agent_position)
-	print("target position: ", nav_agent.get_target_position())
 	var next_path_position:Vector3 = nav_agent.get_next_path_position()
-	print("next_path_position:     ", next_path_position)
 	var new_speed:float = speed * state_machine.current_state.speed_multiplier
-	print("new_speed:              ", new_speed)
 	var new_velo:Vector3 = current_agent_position.direction_to(next_path_position) * new_speed
-	print("new_velo:               ", new_velo)
 	
 	body_velocity = Utils.set_vector_xz(body_velocity, new_velo)
-	
-	print("body_velocity:          ", body_velocity)
-	print()
 
 func set_target(target_global:Vector3) -> void:
 	nav_agent.set_target_position(target_global)
